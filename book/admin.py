6a652df382455ebf8d9ef1cedd3850fbfad9eb89
Avf <Abdurahmon942120510@gmail.com>
2024-09-20 19:03:12 +0500

    category api created

--- book/admin.py
+++ book/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from book.models import Book
+from book.models import Book, Category, Group,Product
+
 
 # Register your models here.
-admin.site.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'price')
+# admin.site.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'price')
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+    list_filter = ('title', 'description')
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+      list_display = ('name', 'description')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+      list_display = ('name',)
--- book/models.py
+++ book/models.py
@@ -12,3 +12,41 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class BaseModel(models.Model):
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+     class Meta:
+         abstract = True
+
+class Category(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Group(BaseModel):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name="groups", on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, related_name="products", on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
